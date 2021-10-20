dbde04521d400f2c32ec0d22c008438accdb4ec0
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2021-10-20 18:34:42 -0300

    criado lógica js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // let prodData;
 
 const start = () => {
-  console.log("app iniciada");
+  fetchData();
   // pegar o html como objeto
   // fazer o fetch do catálogo, filtros, entre outros
   // exibir dados do fetch no html
@@ -19,14 +19,46 @@ const fetchData = () => {
 };
 
 const assembleItems = (fetchData) => {
-  const app = document.getElementById("main");
-  const module = document.createElement("section");
   const data = [...fetchData];
-  data.map((d) => {});
-  module.innerHTML = "testing";
+  data.map((d) => {
+    createItem(d);
+  });
 
-  app.appendChild(module);
-  console.log(`fetched data: ${data}`);
+  //   console.log(`fetched data: ${data}`);
+};
+
+const createItem = (d) => {
+  const img = document.createElement("img");
+  img.src = d.image_link;
+  img.alt = d.description;
+
+  const fig = document.createElement("figure");
+  fig.className = "product-image";
+  fig.appendChild(img);
+
+  const productName = document.createElement("h2");
+  productName.innerText = d.name;
+  productName.className = "product-name";
+
+  const brand = document.createElement("span");
+  brand.innerText = d.brand;
+  brand.className = "product-brand";
+
+  const price = document.createElement("span");
+  price.innerText = d.price;
+  price.className = "product-price";
+
+  const product = document.createElement("section");
+  product.className = "product";
+  product.id = d.id;
+
+  product.appendChild(fig);
+  product.appendChild(productName);
+  product.appendChild(brand);
+  product.appendChild(price);
+
+  const app = document.getElementById("main");
+  app.appendChild(product);
 };
 
 window.addEventListener("load", (e) => {
